0f99c846c8a8ae8e173bdcde31e49c583fdf14f9
algrio santoso <algrio.santoso@gmail.com>
2023-10-17 08:21:35 -0400

    2.2 complete

--- kingc.textbook/kingc2.2.c
+++ kingc.textbook/kingc2.2.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-	printf("To C, or not to C:\n");
-	printf("that is the question. \n");
+	/*printf("To C, or not to C:\n");
+	printf("that is the question. \n");*/
+	printf("Brevity is the soul of wit.\n  --Shakespeare\n");
 	return 0;
 }
