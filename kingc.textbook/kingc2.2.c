fc5edf7023ac41d0d11e7c0d06e7ba1a2d8419e7
algrio santoso <algrio.santoso@gmail.com>
2023-10-19 07:29:21 -0400

    error

--- kingc.textbook/kingc2.2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	/*printf("To C, or not to C:\n");
-	printf("that is the question. \n");*/
-	printf("Brevity is the soul of wit.\n  --Shakespeare\n");
-	return 0;
-}
