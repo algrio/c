f3ca6c544c224c44dfb5b84e0bb14ec1a482c4e3
algrio santoso <algrio.santoso@gmail.com>
2023-10-23 08:39:54 -0400

    kingc 3 pp3

--- kingc.textbook/kingc3.pp2.c
+++ kingc.textbook/kingc3.pp2.c
@@ -2,11 +2,17 @@
 
 int main(void)
 {
-int mm, dd , yyyy;
+int item, mm, dd , yyyy;
+float price;
 
-printf("Enter a date (mm/dd/yyyy): ");
+printf("Enter item number: ");
+scanf("%d", &item);
+printf("Enter unit price: ");
+scanf("%f", &price);
+printf("Enter purchase date (mm/dd/yyyy): ");
 scanf("%d/%d/%d", &mm, &dd, &yyyy);
-printf("You entered the date %d%d%d\n", yyyy, mm, dd);
+
+printf("Item\t\tUnit\t\tPurchase\nNumber\t\tPrice\t\tDate\n%-d\t\t$%7.2f\t%d/%d/%d\n", item, price, yyyy, mm, dd);
 
 return 0;
 }
