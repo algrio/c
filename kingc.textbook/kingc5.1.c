47e5d95dd56ecfbe5c92a406af5f2f0014909cd2
algrio santoso <algrio.santoso@gmail.com>
2023-10-26 06:54:32 -0400

    king c 5.1

---
+++ kingc.textbook/kingc5.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+int w = -1, x =0, y =1, z = 2;
+
+/* if (y < x)
+printf("True\n"); */
+
+/* printf("%d\n", x > y); */
+
+/* if (y != x)
+printf("True\n"); */
+
+/* if (y != x)
+printf("True\n"); */
+
+if (!x && !y)
+printf("True\n");
+
+
+
+return 0;
+}
