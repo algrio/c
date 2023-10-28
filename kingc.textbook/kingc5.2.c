c49a133bbc0cee90e481915a78e71ff0f8b6dd82
algrio santoso <algrio.santoso@gmail.com>
2023-10-28 11:58:39 -0400

    king c 5 2 complete

--- kingc.textbook/kingc5.2.c
+++ kingc.textbook/kingc5.2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
-int x = 0, y = 7;
+int x = 5, y = 4, z = 3, max;
+float trade, commission;
+_Bool test = false;
 
 /* if (0 <= x && x < 8) printf("EQUAL\n"); */
 
@@ -19,6 +22,41 @@ printf("%d\n", x); */
     printf("INEQUAL\n");
     else
     printf("EQUAL\n"); */
+
+/* if (x > y)
+    if (x > z)
+        max = x;
+    else 
+        max = z;
+else
+    if (y > z)
+        max = y;
+    else
+        max = z;
+printf("%d\n", max); */
+
+/* printf("Enter Trade Value : USD ");
+scanf("%f", &trade);
+
+if (trade < 2500)
+    commission = 30 + .017 * trade;
+else if (trade < 6250)
+    commission = 56 + .0066 * trade;
+else if (trade < 20000)
+    commission = 76 + .0034 * trade;
+else if (trade < 50000)
+    commission = 100 + .0022 * trade;
+else if (trade < 500000)
+    commission = 155 + .0011 * trade;
+else
+    commission = 266 + .0009 * trade;
+
+if (commission < 39.0f)
+    commission = 39.f;
+
+printf("Commission : USD %.2f\n", commission); */
+
+/* if (test) printf ("%d\n",test); */
 
 return 0; 
 }
