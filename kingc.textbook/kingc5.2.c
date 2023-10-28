77343891a2d279a4e25bc37ef6294edc89760910
algrio santoso <algrio.santoso@gmail.com>
2023-10-28 09:30:28 -0400

    kingc 5 2

--- kingc.textbook/kingc5.2.c
+++ kingc.textbook/kingc5.2.c
@@ -2,17 +2,23 @@
 
 int main(void)
 {
-int x = 0, y = 77777;
+int x = 0, y = 7;
 
 /* if (0 <= x && x < 8) printf("EQUAL\n"); */
 
 /* if (x <= 0 || 8 < x) printf("EQUAL\n"); */
 
-while (x != y)
+/* while (x != y)
     {
     printf("%d\n", x);
     ++x;
     }
-printf("%d\n", x);
-return 0;
+printf("%d\n", x); */
+
+/* if (x != y)
+    printf("INEQUAL\n");
+    else
+    printf("EQUAL\n"); */
+
+return 0; 
 }
