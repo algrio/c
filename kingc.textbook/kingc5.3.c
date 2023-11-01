a0ba10b9f99cdfb154790e2ae8d0c2ec2d0a603d
algrio santoso <algrio.santoso@gmail.com>
2023-11-01 10:17:48 -0400

    king c 5 3

--- kingc.textbook/kingc5.3.c
+++ kingc.textbook/kingc5.3.c
@@ -3,15 +3,5 @@
 
 int main(void)
 {
-bool x = 100;
-
-while (x != 0)
-    {
-    if (x > 0)
-    {printf("True %d", x); --x;}
-    else if (x < 0) 
-    {printf("True %d", x); ++x;}
-    }
-
 return 0; 
 }
