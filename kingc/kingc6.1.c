5a6b2549cc2c7c1ae562a5fe6c00e718627d19fd
algrio santoso <algrio.santoso@gmail.com>
2023-11-05 16:56:48 -0500

    king c 6 2

--- kingc/kingc6.1.c
+++ kingc/kingc6.1.c
@@ -3,5 +3,33 @@
 
 int main(void)
 {
+int i=1, n, sum = 0;
+
+/* while (i < n) 
+{
+i=i*2;
+printf("%d\n", i);
+} */
+
+/* printf("This program prints a table of squares.\nEnter The number of entries : ");
+scanf("%d", &n);
+
+while (i <= n)
+{
+    printf("%10d %10d\n", i, i*i);
+    ++i;
+} */
+
+printf("This program sums a series of integers.\nEnter Integers : ");
+
+scanf("%d", &n);
+while (n != 0)
+{
+    sum += n;
+    scanf("%d", &n);
+}
+
+printf("Sum : %d\n", sum);
+
 return 0;
 }
