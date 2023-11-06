4994a540d1c76f0f9ba8957b1f9be1a2ad22ab78
algrio santoso <algrio.santoso@gmail.com>
2023-11-06 15:51:08 -0500

    king c 6 3

--- kingc/kingc6.2.c
+++ kingc/kingc6.2.c
@@ -3,8 +3,32 @@
 
 int main(void)
 {
-int i, n, sum;
+int i = 0, n, sum;
 
+/* printf("Enter Number For Countdown: ");
+scanf("%d", &n);
+
+do
+{
+    printf("%d\n", n);
+    --n;
+}
+while (n > i); */
+
+printf("Enter An Integer : ");
+scanf("%d", &n);
+
+do
+{
+    n /= 10;
+    i++;
+}
+while (n > 0);
+
+if (i == 1)
+    printf("Integer Entered Has %d Digit\n", i);
+else
+    printf("Integer Entered Has %d Digits\n", i);
 
 return 0;
 }
---
+++ kingc/kingc6.3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+for(int x = 1000000; x >= 0; x--)
+    printf("%7d\n", x);
+return 0;
+}
